3f01c88eb3e2565c4734d5670223e4fd99bee494
strgtscntst <66039310+strgtscntst@users.noreply.github.com>
2024-06-19 12:12:43 -0400

    fix input bugs
    Fixed bug where repeating "=" did not repeat the last operation
    Fixed bug where the "C" button did not properly clear the opString
    Fixed bug where the "CE" button did not properly clear the opString
    Fixed some minor bugs resulting from improperly placed flags

--- calculator.js
+++ calculator.js
@@ -15,29 +15,38 @@ BUTTONS.forEach((node) => {
 });
 
 
-console.log("Hi!")
-console.log(BUTTONS)
-let reset = true;
-let opReset = true;
-let fullReset = false;
+
+//flags
+let reset = true;       //flag signals readiness for number input
+let opReset = true;     //flag signals end of operation stored for repeated "=" use
+let fullReset = false;  //flag for "C" button
 
 function buttonPress(thisThing){
     console.log(thisThing.textContent)
 
     //Add buttonpress to end of calcString
-    if(calcString == "" && thisThing.textContent[0] == " "){calcString = inputDisplay.textContent}
+    if(calcString == "" && thisThing.textContent[0] == " "){
+        calcString = inputDisplay.textContent}
 
     calcString = calcString + thisThing.textContent;
-    if(thisThing.textContent != " ="){opString = opString + thisThing.textContent;}
+    if(thisThing.textContent != " =" && opString.length >= 1){
+        opString = opString + thisThing.textContent;}
 
     //Protect against double-operators, replace with most recent operator
     if(calcString.search("  ") != -1){
         calcString = calcString.slice(0, calcString.length - 6) + calcString.slice(calcString.length - 3)
     }
+    if(opString.search("  ") != -1){
+        opString = opString.slice(0, opString.length - 6) + opString.slice(opString.length - 3)
+    }
+
+    //Protct against multiple decimal points
+    
 
     //Operations based on last button pressed
     switch(calcString[calcString.length - 1]){
-        case " ":
+        case " ":   //Whitespace used to denote operators
+                    //Also used for .split() for processing in runCalc()
             inputDisplay.textContent = runCalc(calcString);
             fullDisplay.textContent =  calcString;
             reset = true;
@@ -46,24 +55,30 @@ function buttonPress(thisThing){
                 opString = calcString.slice(calcString.length - 3);
             }
             opReset = false;
+            fullReset = false;
             break;
-        case "E":
+        case "E":   //"CE" or "Clear Entry" button, basically backspace
             switch(calcString[calcString.length - 3]){
                 case " ":
                     calcString = calcString.slice(0, calcString.length - 5);
+                    opString = opString.slice(0, opString.length - 5);
                     break;
-                default: calcString = calcString.slice(0, calcString.length - 3)
+                default: 
+                    calcString = calcString.slice(0, calcString.length - 3)
+                    opString = opString.slice(0, opString.length - 3)
             }
             break;
-        case "C":
+        case "C":   //"Clear" button
             calcString = "";
-            inputDisplay.textContent = ""
-            fullDisplay.textContent = ""
+            opString = "";
+            inputDisplay.textContent = "";
+            fullDisplay.textContent = "";
             break;
         case "=":
             //if enter's already been pressed, repeat last operation
             if(Array.from(calcString).filter((c) => c == "=").length == 2){
-                calcString = repeatOp(calcString, opString)
+                calcString = repeatOp(calcString, opString);
+                // calcString = calcString.slice(0, calcString.length - 2)
             }
             
             inputDisplay.textContent = runCalc(calcString)
@@ -71,14 +86,14 @@ function buttonPress(thisThing){
             fullReset = true;
             reset = true;
             break;
-        default:
-            if(reset){inputDisplay.textContent = ""}
-            inputDisplay.textContent = inputDisplay.textContent + calcString[calcString.length - 1]
+        default:    //digit inputs
+            if(reset){inputDisplay.textContent = ""};
+            inputDisplay.textContent = inputDisplay.textContent + calcString[calcString.length - 1];
             reset = false;
             opReset = true;
             if(fullReset){
                 calcString = calcString[calcString.length - 1];
-                
+                opString = opString[opString.length - 1];
             }
             fullReset = false;
         }
